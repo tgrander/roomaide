d0454f7ae9c6b8cc1434b2aae630a04013fff6dc
tgrander <treygranderson@gmail.com>
2016-07-06 16:26:46 -0400

    further updates to server file

--- roomaide.js
+++ roomaide.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(express.static(__dirname + "/../client"));
+app.use(express.static(__dirname + "/../client/index.html"));
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('Server is running');
