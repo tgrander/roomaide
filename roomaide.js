1feea1c61c69ac7e0f3582ddf0c554bb3e75633c
tgrander <treygranderson@gmail.com>
2016-07-06 16:24:01 -0400

    Fixed server file;

--- roomaide.js
+++ roomaide.js
@@ -5,6 +5,6 @@ var app = express();
 
 app.use(express.static(__dirname + "/../client"));
 
-app.listen(process.env.PORT ||| 3000, () => {
+app.listen(process.env.PORT || 3000, function(){
   console.log('Server is running');
 });
