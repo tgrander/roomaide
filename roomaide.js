69cb6bf913d00ee50cadf3b1f14e831d5c82f4dc
tgrander <treygranderson@gmail.com>
2016-07-06 17:05:10 -0400

    Deployed locally

--- roomaide.js
+++ roomaide.js
@@ -3,7 +3,13 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(express.static(__dirname + "/../client/index.html"));
+app.use(express.static(__dirname + "/src/client"));
+app.use(express.static(__dirname + "/node_modules"));
+
+
+// app.get('/', function(req, res){
+//   res.render('index')
+// })
 
 app.listen(process.env.PORT || 3000, function(){
   console.log('Server is running');
